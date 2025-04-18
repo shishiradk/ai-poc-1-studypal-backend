c0636f01a4af12470d2203657d0b2c46d59a49c4
Shishir <adhikarishishir11@gmail.com>
2025-04-19 03:03:34 +0545

    using groq llm

--- get_llm_response.py
+++ get_llm_response.py
@@ -1,27 +1,23 @@
-#from dotenv import load_dotenv
-#from langchain_groq import ChatGroq
-from langchain_ollama import ChatOllama
-#load_dotenv()
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+# from langchain_ollama import ChatOllama
 
+load_dotenv()
 
-#llm = ChatGroq(
-#    model="llama-3.1-8b-instant",
-#     temperature=0,
-#     max_tokens=None,
-#     timeout=None,
-#     max_retries=2,
-# )
-
-llm = ChatOllama(
-    model="deepseek-r1:1.5b",
-    temperature=0
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
 )
 
+# llm = ChatOllama(
+#     model="deepseek-r1:1.5b",
+#     temperature=0
+# )
+
 def get_answer(question):
-    answer = llm.invoke("What is ML")
+    answer = llm.invoke(question)
     answer = answer.content
     return answer
-
-
-
-
